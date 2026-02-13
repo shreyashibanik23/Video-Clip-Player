f757307d2126b83398f743532a365d238fb99281
Shreyashi Banik <shreyashibanik50@gmail.com>
2026-02-13 13:17:41 +0530

    Prepared backend for deployment

--- backend/app.py
+++ backend/app.py
@@ -33,5 +33,9 @@ def get_video(filename):
     return send_from_directory(app.config["UPLOAD_FOLDER"], filename)
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
